5248da5142bd8f51acc80715f35ed238f638b8da
Giovanni Kuan <giokuan@gmail.com>
2024-11-12 18:59:25 +0800

    Removed the register in homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,19 +44,23 @@
                                 @else
                                     <a
                                         wire:navigate href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-yellow-500 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                                        class="flex rounded-md px-3 py-2 text-yellow-500  ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                                     >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                                      </svg>
+                                      
                                         Log in
                                     </a>
 
-                                    @if (Route::has('register'))
+                                    {{-- @if (Route::has('register'))
                                         <a
                                             wire:navigate href="{{ route('register') }}"
                                             class="rounded-md px-3 py-2 text-yellow-500 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                                         >
                                             Register
                                         </a>
-                                    @endif
+                                    @endif --}}
                                 @endauth
                             </nav>
                         @endif
