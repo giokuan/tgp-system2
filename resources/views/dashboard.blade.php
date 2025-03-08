24869d770f6ab287afaf7ac1cd5479649b550f45
Giovanni Kuan <giokuan@gmail.com>
2025-03-08 22:25:31 +0800

    edit the dashboard removes unnecessary comments

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,8 @@
 <x-app-layout>
-    {{-- <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight mt-16">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot> --}}
-    {{-- <x-welcome></x-welcome> --}}
+
 
         <livewire:report-status />
   
-        {{-- <livewire:slider-logo /> --}}
 
 
 
