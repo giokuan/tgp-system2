3fec98ecf4c2ecd2caba32055aaaaa7f648c1654
Giovanni Kuan <giokuan@gmail.com>
2025-03-13 23:42:06 +0800

    remove the filament exixting widget

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,3 +1,14 @@
 <x-filament-panels::page>
+    <div class="flex items-center gap-x-3">
+        <div  class="flex-1 ">
+            <p class=" font-bold text-lg">Important Reminder for Moderators</p><br>
+            <span class="font-bold ">Please be advised:</span> As a moderator, you have access to sensitive information within this system. 
+            Every action you take is crucial in ensuring the integrity, privacy, and trust within our brotherhood. 
+            Please handle all data with care and responsibility, keeping in mind the impact your actions may have on fellow members.<br><br>
 
+            <span class="font-bold ">Policy Note:</span> Unauthorized sharing, editing, or misuse of sensitive data is strictly 
+            prohibited and may lead to consequences aligned with our community values. Thank you for your commitment to upholding the 
+            trust and respect within our brotherhood.
+        </div>
+    </div>
 </x-filament-panels::page>
